9e81c48d88013d06b91d34cb4a39382d35f4e957
Tacheng <tacheng9502@gmail.com>
2016-06-16 13:34:05 +0800

    修正錯誤

--- js/all.js
+++ js/all.js
@@ -257,6 +257,8 @@ function Dialog(dataCount) {
 
     var startDateTime = selectData[dataCount].showInfo[0].time.replace(" ", "T");
     var endDateTime = selectData[dataCount].showInfo[0].endTime.replace(" ", "T");
+    startDateTime = startDateTime.replace(/\//g, "-");
+    endDateTime = endDateTime.replace(/\//g, "-");
     console.log(startDateTime);
     console.log(endDateTime);
 
