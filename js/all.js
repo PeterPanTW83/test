1be5a5fd0cbe1b4a38b7a8b1d225deaacd9e99be
beibeihuang <ya0981012155@yahoo.com.tw>
2016-06-16 15:42:09 +0800

    Update all.js

--- js/all.js
+++ js/all.js
@@ -257,7 +257,7 @@ function createEvent() {
     request.execute(function(event) {
         swal({
             title: "完成",
-            text: "活動已經新增至您的日曆中<br><a href='" + event.htmlLink + "'>查看日曆</a>",
+            text: "活動已經新增至您的日曆中<br><a href='" + event.htmlLink + "' target="_new">查看日曆</a>",
             type: "success",
             closeOnConfirm: true,
             html: true
