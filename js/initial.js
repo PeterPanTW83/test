08a6f4eafad4a0e7e230cd6af68a7533cd0b94e9
Tacheng <tacheng9502@gmail.com>
2016-06-18 12:26:38 +0800

    修正錯誤

---
+++ js/initial.js
@@ -0,0 +1,19 @@
+require([
+    'https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js',
+    'https://maps.googleapis.com/maps/api/js?key=AIzaSyA0UyOx-fxoHQGbHLnU9475buiCyfR28kQ',
+    'https://apis.google.com/js/client.js',
+    'js/sweetalert.min.js',
+    'js/taiwan-city-selector.js',
+    'js/all.js'
+
+], function() {
+
+    initMap();
+
+    $('.btn-filter').on('click', function() {
+        $('.filter').toggle();
+    });
+
+    $('#form').dk_tw_citySelector('#city', '#district', '#zipcode');
+
+});
