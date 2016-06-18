0f00707504e5350a804f519f886ae46e4c99c650
Tacheng <tacheng9502@gmail.com>
2016-06-18 13:34:28 +0800

    嘗試城市

--- js/initial.js
+++ js/initial.js
@@ -1,12 +1,20 @@
-require([
-    'https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js',
-    'https://maps.googleapis.com/maps/api/js?key=AIzaSyA0UyOx-fxoHQGbHLnU9475buiCyfR28kQ',
-    'js/oms.min.js',
-    'https://apis.google.com/js/client.js',
-    'js/sweetalert.min.js',
-    'js/taiwan-city-selector.js',
-    'js/all.js'
-], function() {
+require.config({
+    paths: {
+        jquery: 'https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min',
+        googleMap: 'https://maps.googleapis.com/maps/api/js?key=AIzaSyA0UyOx-fxoHQGbHLnU9475buiCyfR28kQ',
+        oms: 'js/oms.min',
+        googleClient: 'https://apis.google.com/js/client',
+        sweetAlert: 'js/sweetalert.min',
+        taiwanCity: 'js/taiwan-city-selector',
+        all: 'js/all'
+    },
+    shim: {
+        'taiwanCity': ['jquery'],
+        'all': ['oms']
+    }
+});
+
+require(['jquery', 'googleMap', 'oms', 'googleClient', 'sweetAlert', 'taiwanCity', 'all'], function() {
 
     initMap();
 
