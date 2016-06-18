9af4dd2393b25a7ac6895703ea76edd16dbcb260
Tacheng <tacheng9502@gmail.com>
2016-06-18 13:23:43 +0800

    修正錯誤

--- js/initial.js
+++ js/initial.js
@@ -1,12 +1,12 @@
 require.config({
     paths: {
-        'jquery': 'https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js',
-        'googleMap': 'https://maps.googleapis.com/maps/api/js?key=AIzaSyA0UyOx-fxoHQGbHLnU9475buiCyfR28kQ',
-        'oms': 'js/oms.min.js',
-        'googleClient': 'https://apis.google.com/js/client.js',
-        'sweetAlert': 'js/sweetalert.min.js',
-        'taiwanCity': 'js/taiwan-city-selector.js',
-        'all': 'js/all.js'
+        jquery: 'https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js',
+        googleMap: 'https://maps.googleapis.com/maps/api/js?key=AIzaSyA0UyOx-fxoHQGbHLnU9475buiCyfR28kQ',
+        oms: 'js/oms.min.js',
+        googleClient: 'https://apis.google.com/js/client.js',
+        sweetAlert: 'js/sweetalert.min.js',
+        taiwanCity: 'js/taiwan-city-selector.js',
+        all: 'js/all.js'
     },
 
 });
