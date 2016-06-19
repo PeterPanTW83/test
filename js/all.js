fbda3815c3a639debc6978442f854e48a044cab9
beibeihuang <ya0981012155@yahoo.com.tw>
2016-06-19 11:13:34 +0800

    Update all.js

--- js/all.js
+++ js/all.js
@@ -247,7 +247,8 @@ function search() {
 
     searchResult = [];
     var tempData = [];
-
+    
+    $('.info').find('img').attr('src','img/banner-search.png');
     $('#list').html("");
     deleteMarkers();
 
