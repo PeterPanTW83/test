548c02df30f7c09475b7482c8c89ed7f8c09591a
beibeihuang <ya0981012155@yahoo.com.tw>
2016-06-19 13:39:44 +0800

    Update all.js

--- js/all.js
+++ js/all.js
@@ -8,6 +8,7 @@ var isSearch = false;
 var jsonData;
 var searchResult
 var oms;
+var userPosition;
 
 function initMap() {
     map = new google.maps.Map(document.getElementById('map-list'), {
@@ -104,7 +105,7 @@ function showData(data) {
 
         createMarkers(i, dataCoordinates, dataTitle, dataStartTime, dataEndTime, dataLocation, dataLocationName, dataCategory, dataFavoriteHtml);
 
-        $('#list').append('<li><a href="javascript:focusLocation(\'' + i + '\')" class="clearfix"><img src="' + dataImageUrl + '" class="photo"><div class="activity-info"><h2>' + dataTitle + '</h2><ul><li><i class="fa fa-clock-o fa-lg" aria-hidden="true"></i>' + dataStartTime + '~' + dataEndTime + '</li><li><i class="fa fa-home fa-lg" aria-hidden="true"></i>' + dataLocationName + '</li><li><i class="fa fa-map-marker fa-lg" aria-hidden="true"></i>' + dataLocation + '</li></ul><div class="activity-btn"><button onclick="changeFavorite(\'' + i + '\', $(this), true)" class="favorite">' + dataFavoriteHtml + '</button><button onclick="" class="route"><img src="img/route.png">路線規劃</button><button onclick="Dialog(\'' + i + '\')" class="add-calendar"><img src="img/min-calendar.png">加至Google日曆</button></div></div></a></li>');
+        $('#list').append('<li><a href="javascript:focusLocation(\'' + i + '\')" class="clearfix"><img src="' + dataImageUrl + '" class="photo"><div class="activity-info"><h2>' + dataTitle + '</h2><ul><li><i class="fa fa-clock-o fa-lg" aria-hidden="true"></i>' + dataStartTime + '~' + dataEndTime + '</li><li><i class="fa fa-home fa-lg" aria-hidden="true"></i>' + dataLocationName + '</li><li><i class="fa fa-map-marker fa-lg" aria-hidden="true"></i>' + dataLocation + '</li></ul><div class="activity-btn"><button onclick="changeFavorite(\'' + i + '\', $(this), true)" class="favorite">' + dataFavoriteHtml + '</button><button onclick="https://maps.google.com/?saddr="' + userPosition + '?daddr="' + dataLocation + 'class="route"><img src="img/route.png">路線規劃</button><button onclick="Dialog(\'' + i + '\')" class="add-calendar"><img src="img/min-calendar.png">加至Google日曆</button></div></div></a></li>');
     }
 }
 
@@ -413,13 +414,13 @@ function geoFindMe() {
     }
 
     function success(position) {
-        var pos = {
+        userPosition = {
             lat: position.coords.latitude,
             lng: position.coords.longitude
         };
-        infoWindow.setPosition(pos);
+        infoWindow.setPosition(userPosition);
         infoWindow.setContent('<div class="activity-info"><h2>你的位置</h2><ul><li><i class="fa fa-home" aria-hidden="true"></i>這裡放定位地址</li></ul></div>');
-        map.setCenter(pos);
+        map.setCenter(userPosition);
     };
 
     function error() {
